000b8ac5c84239c3116268a1b198e1b80b5023cd
vipul73921 <36297614+vipul79321@users.noreply.github.com>
2021-01-04 18:10:04 +0530

    Create diameter_of_tree_using_single_dfs.cpp

---
+++ diameter_of_tree_using_single_dfs.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ull unsigned long long
+#define mp make_pair
+#define ll long long
+#define pb push_back
+
+int diameter(int u, int par, vector<vector<int> >&adj, int* curr_max)
+{
+    int max1 = 0, max2 = 0, total = 0;
+
+    for(auto v:adj[u])
+    {
+        if(v==par)continue;
+
+        total = max(total, diameter(v,u,adj,curr_max));
+
+        if(*curr_max>max1)
+        {
+            max2 = max1;
+            max1 = *curr_max;
+        }
+        else
+            max2 = max(max2, *curr_max);
+    }
+
+    total = max(total, max1 + max2); // total will maintain length of the longest path in subtree rooted at that node.
+ 
+    *curr_max = max1 + 1;   // curr_max will maintain length of longest path in subtree rooted at that node and starting from that node.
+
+    return total;
+
+}
+
+void solve(int test)
+{
+   int n;
+   cin>>n;
+
+   vector<vector<int> >adj(n);
+
+   int i,j;
+
+   for(i=0;i<n-1;i++)
+   {
+       int a,b;
+       cin>>a>>b;
+       adj[a].pb(b);
+       adj[b].pb(a);
+   }
+
+   int curr_max = 0;
+   int diam = diameter(0,-1,adj,&curr_max);
+
+   cout<<diam<<endl;
+
+}
+
+int main()
+{
+
+  int t = 1;
+  //cin>>t;
+
+  for(int x=1;x<=t;x++)
+  {
+      solve(x);
+  }
+  return 0;
+}
+
+
+/*
+sample input
+8
+0 7
+1 5
+2 0
+4 2
+6 7
+7 3
+7 5
+*/
